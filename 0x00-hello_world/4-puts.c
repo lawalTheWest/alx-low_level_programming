4eaf68e69d9bb0a137e6a2a5dc6eb9c2ae931c08
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-16 11:14:05 -0800

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+gcc $CFILE -o cisfun
+root@2e845333a187:/alx-low_level_programming/0x00-hello_world# cat 4-puts.c
+#include <stdio.h>
+/*
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		                return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+*main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		                return (0);
+}
+root@2e845333a187:/alx-low_level_programming/0x00-hello_world# cat 5-printf.c
+#include <stdio.h>
+/*
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	        printf("with proper grammar, but the outcome is a piece of art,\n");
+		                return (0);
+}

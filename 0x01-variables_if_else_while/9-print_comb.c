fadf02bc082639b68e3987fd87a077b123700b64
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-17 20:28:05 -0800

    prints single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'prints alphabet in reverse'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar(j);
+		if (j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

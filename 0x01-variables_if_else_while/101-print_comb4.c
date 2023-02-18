e642853ea6bda9e55f93e7643b43dc496261d33a
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-17 22:32:11 -0800

    printing number combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'prints combination of number'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i < 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

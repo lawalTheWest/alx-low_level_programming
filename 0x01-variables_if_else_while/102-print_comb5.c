ecaec66204808c4aa146634477c92f7d820a3204
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-17 22:57:43 -0800

    printing numbers combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'prints combination of number'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n1,  n2;
+
+	for (n1 = 0; n1 < 100; n1++)
+	{
+		for (n2 = n1; n2 < 100; n2++)
+		{
+			/* Print the first number */
+			putchar('0' + n1 / 10);
+			putchar('0' + n1 % 10);
+			putchar(' ');
+			/* Print the second number */
+			putchar('0' + n2 / 10);
+			putchar('0' + n2 % 10);
+			if (n1 != 99 || n2 != 99)
+			{
+				/* If this is not the last combination, print the separator */
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

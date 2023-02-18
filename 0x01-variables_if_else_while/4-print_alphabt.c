b3116904140fc69e9aba82eb8c58f28277a5f388
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-17 19:05:18 -0800

    prrint alphabet with the exception of e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: 'this program prints the alphabet in  lowercase'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		if (j == 'Q' || j == 'E')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(tolower(j));
+		}
+	}
+	putchar('\n');
+	return (0);
+}

1fb29f530f8be0d24edb42df4dfe54e389e72176
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-21 13:30:34 -0800

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,9 @@
 #include "main.h"
-
 /**
- *  print_alphabet_x10 - Entry point 
- *  Description: 'print alphabet'
- *
- *   Return: Always 0 (success)
- *
- */
-
+* print_alphabet_x10 - Entry point
+* Description: 'print alphabet'
+* Return: Always 0 (success)
+*/
 void print_alphabet_x10(void)
 {
 	int alphabet;

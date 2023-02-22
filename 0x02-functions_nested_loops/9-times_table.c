64dfbf6db677185410b79ec49cdafb5ae0d550b7
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-22 15:34:53 -0800

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * times_table - Entry point
- * Description: 'Prints the 9 times table, Starting with 0'
- * Return: Always 0 (success)
+ * times_table - Prints the 9 times table, Starting with 0
+ *
+ * Return: empty output
  */
 
 void times_table(void)
@@ -14,15 +14,12 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
-
 		{
 			c = a * b;
-
 			if (c > 9)
 			{
 				d = c % 10;
 				e = (c - d) / 10;
-
 				_putchar(44);
 				_putchar(32);
 				_putchar(e + '0');
@@ -30,16 +27,15 @@ void times_table(void)
 			}
 			else
 			{
-
 				if (b != 0)
 				{
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
-					_putchar(c + '0');
 				}
+				_putchar(c + '0');
 			}
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }

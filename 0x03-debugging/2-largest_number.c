dc5e4ea9b78d786f0e103080658cb7ad2ead5387
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-02-23 20:57:51 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,14 +12,16 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
+
 	else
 	{
 		largest = c;

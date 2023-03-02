bebd9c32d5564de67f88a73e67ea001c8c7c6aed
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-03-03 00:03:48 +0100

    the dream numbers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -24,7 +24,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  
+
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);

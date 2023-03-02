ae5ca4f6106ef7bb867962188612c6b74c279ea5
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-03-02 23:54:18 +0100

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @num: int to print
+ * Return: void
+ */
+
+void print_number(int num)
+{
+	unsigned int d = 10, n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
+}

a6963c2708506db2bbd8cd0f9188c613437695ed
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-03-02 22:08:23 +0100

    pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strcat - Entry point
+ * Description: 'concatenates two strings'
  * @dest: A pointer to a character that will be changed
  * @src: A pointer to a character that will also be changed
  * Return: dest

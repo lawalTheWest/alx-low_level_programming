e790eaf48d0d7b1bb79cc19c8890eaf87fe48adf
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-03-06 14:46:07 +0100

    A function that prints the sum of the two diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ * Description: 'prints the sum of the two diagonals'
+ * @a: 2d array of int types
+ * @size: size of array (square)
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		sum2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}

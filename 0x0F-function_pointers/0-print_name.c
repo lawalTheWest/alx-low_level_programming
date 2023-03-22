f5682714aed573a8ac86a2458ac4bf8f23d27515
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-03-22 21:06:04 +0100

    a function that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: pointer to the name printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}

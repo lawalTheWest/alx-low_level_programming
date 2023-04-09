75c48c8b5795a130e34e2db30c0b367105daf4bc
Lawal Tajudeen <lwltjdn@gmailcom>
2023-04-10 00:58:19 +0100

    update on header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,11 +10,12 @@
 #include <stdlib.h>
 
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
 
 int create_file(const char *filename, char *text_content);
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
+int _putchar(char c);
 
 void print_magic(unsigned char *e_ident);
 

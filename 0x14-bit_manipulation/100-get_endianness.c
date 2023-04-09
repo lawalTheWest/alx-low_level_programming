ec1b713bbb3c884deda9fc2bce26fea0a0c233c4
Lawal Tajudeen <lwltjdn@gmailcom>
2023-04-09 14:12:39 +0100

    getting the order in which a sequence of bytes is stored in memory

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+
+{
+	/* 
+	 * the endian term that describes the order in which a sequence of bytes is stored in  memory.
+	 */
+	int endianValue = 1;
+	char *y;
+
+	y = (char *)&endianValue;
+
+	return (*y);
+}

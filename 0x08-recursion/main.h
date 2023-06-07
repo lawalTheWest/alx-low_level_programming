9fdcaa69327e395ed61f41b54724684f30500b36
lawal tajudeen <lwltjdn@gmail.com>
2023-06-07 09:48:45 +0100

    custom header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/*
+ * prototype of a function that prints a string,
+ * followed by a new line.
+ * task #0
+ */
+void _puts_recursion(char *s);
+
+#endif

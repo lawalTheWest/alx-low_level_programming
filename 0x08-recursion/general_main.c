8856a45a3ada04e4c7f261b1d318f59e6daf4e71
lawal tajudeen <lwltjdn@gmail.com>
2023-06-07 12:47:35 +0100

    custom test file

--- 0x08-recursion/general_main.c
+++ 0x08-recursion/general_main.c
@@ -1,14 +1,24 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - entry point
  * Return: Zero (0)
  */
 int main(void)
 {
+	int n;
+
 	/* to test task #0 */
 	_puts_recursion("This is my code to test my created function.");
+
 	/* to test task #1 */
 	_print_rev_recursion("\nThis is my code to test my created function.");
+
+	/* to test task #2 */
+	n = _strlen_recursion("This is my code to test my created function.");
+	printf("%i \n", n);
+	_putchar(48 + n);
+	_putchar('\n');
+
 	return (0);
 } /* End main */

a4f802e191fb8ee82d350d4436e455bcee957108
lawal tajudeen <lwltjdn@gmail.com>
2023-06-07 10:27:49 +0100

    Custom codes to test the custom function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Zero (0)
+ */
+int main(void)
+{
+	_puts_recursion("This is my code to test my created function.");
+	return (0);
+} /* End main */

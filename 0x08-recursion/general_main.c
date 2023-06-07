b51dfafc8e13d4afc2d80f06762cde78b1ec62c1
lawal tajudeen <lwltjdn@gmail.com>
2023-06-07 11:09:45 +0100

    custom test file

---
+++ 0x08-recursion/general_main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Zero (0)
+ */
+int main(void)
+{
+	/* to test task #0 */
+	_puts_recursion("This is my code to test my created function.");
+	/* to test task #1 */
+	_print_rev_recursion("\nThis is my code to test my created function.");
+	return (0);
+} /* End main */

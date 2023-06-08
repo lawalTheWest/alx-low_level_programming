fed05c1f57a4dab12414e630f55556cb87e81832
lawal tajudeen <lwltjdn@gmail.com>
2023-06-09 00:25:51 +0100

    A  function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,11 @@
 #define bool int
 #define true 1
 #define false 0
+
+/* Function Prototypes */
+int _strlen_recursion(char *s);
+int _palindrome_truthfullness(char *s, int lenght, int counter);
+
 /**
  * is_palindrome - Entry point
  * Description: 'to check a string palindrome status'
@@ -32,3 +37,48 @@ int is_palindrome(char *s)
 	 */
 	return (_palindrome_truthfullness(s, lenght, counter));
 } /* end function is_palindrome */
+
+
+/**
+ * _strlen_recursion - Entry point
+ * Return: lenght of string
+ * @s: pointer to string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		/* increment the lenght */
+		len += 1;
+		/* Recursion call */
+		len += _strlen_recursion(s += 1);
+	} /* end if */
+
+	return (len);
+} /* End function */
+
+/**
+ * _palindrome_truthfullness - Entry point
+ * Return: zero (false) if check is false
+ * and (true) if true
+ * @s: string
+ * @lenght: string lenght
+ * @counter: index values
+ */
+int _palindrome_truthfullness(char *s, int lenght, int counter)
+{
+	/* divide the string into 2 */
+	if (s[counter] == s[lenght / 2])
+	{
+		/* once the comparism is complete */
+		return (true);
+	} /* End if */
+	else if (s[counter] == s[lenght - counter - 1])
+	{
+		/* recursion call to compare the chars */
+		return (_palindrome_truthfullness(s, lenght, (counter + 1)));
+	} /* end else if */
+	return (false);
+} /* end function */

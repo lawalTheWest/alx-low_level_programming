693c1f5e4b2beb484cdf3ec76b38e416201bdfb8
lawal tajudeen <lwltjdn@gmail.com>
2023-06-13 19:41:33 +0100

    A  program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-
+#define bool int
+#define true 1
+#define SUCCESS 0
 /**
  * main - adds two positive number
  * @argc: number of arguments
@@ -9,75 +11,92 @@
  */
 int main(int argc, char *argv[])
 {
-        int sum, num, i, j, k;
+	/* initialize variables */
+	int sum, num, i, j, k;
 
-        sum = 0;
+	sum = 0; /* this stores the sum of positive numbers */
+	/* validating inputs */
+	for (i = 1; i < argc; i++)
+	{
+		/*
+		 * Iterating over the arguments (argv)
+		 * starting from index 1
+		 * for each argument, we loop through
+		 * each character to check if it is a digit
+		 */
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				/*
+				 * if the adress contains a
+				 * non number character,
+				 * it prints error
+				 */
+				puts("Error");
+				return (true);
+			} /* end if */
+		} /* end for */
+	} /* End for */
+	/* loop through to add the positive numbers */
+	for (k = 1; k < argc; k++)
+	{
+		/* converting the strings to intergers */
+		num = _atoi(argv[k]);
+		/* adding the positive numbers */
+		if (num >= 0)
+			sum += num; /* end if */
+	} /* end for */
+	/* printing the sum */
+	printf("%d\n", sum);
+	return (SUCCESS);
+} /* End main function */
 
-        for (i = 1; i < argc; i++)
-        {
-                for (j = 0; argv[i][j] != '\0'; j++)
-                {
-                        if (argv[i][j] > '9' || argv[i][j] < '0')
-                        {
-                        puts("Error");
-                        return (1);
-                        }
-                }
-        }
-
-        for (k = 1; k < argc; k++)
-        {
-                num = _atoi(argv[k]);
-                if (num >= 0)
-                {
-                        sum += num;
-                }
-        }
-
-        printf("%d\n", sum);
-        return (0);
-}
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-        int i, d, n, len, f, digit;
+	int i = 0; /* index */
+	int d = 0; /* counter */
+	int n = 0; /* converted string to integer */
+	int len = 0;/* string lenght */
+	int f = 0; /* consecutive numbers */
+	int digit = 0; /* curent value */
 
-        i = 0;
-        d = 0;
-        n = 0;
-        len = 0;
-        f = 0;
-        digit = 0;
-
-        while (s[len] != '\0')
-                len++;
-
-        while (i < len && f == 0)
-        {
-                if (s[i] == '-')
-                        ++d;
-
-                if (s[i] >= '0' && s[i] <= '9')
-                {
-                        digit = s[i] - '0';
-                        if (d % 2)
-                        digit = -digit;
-                        n = n * 10 + digit;
-                        f = 1;
-                        if (s[i + 1] < '0' || s[i + 1] > '9')
-                                break;
-                        f = 0;
-                }
-                i++;
-        }
-
-        if (f == 0)
-        return (0);
-
-        return (n);
-        }
+	/* getting the lenght of input */
+	while (s[len] != '\0')
+	{
+		len++;
+	} /* end while */
+	while (i < len && f == 0)
+	{
+		/* if character is negative */
+		if (s[i] == '-')
+			++d; /* end if */
+		/* validating input to be digits */
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			/* checking the ASCII equivalent */
+			digit = s[i] - '0';
+			/* if count is odd, make the value negative */
+			if (d % 2)
+				digit = -digit; /* End if */
+			n = n * 10 + digit;
+			f = 1; /* indicate the processing of a value */
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			} /* end if */
+			f = 0;
+		} /* end if */
+		i++;
+	} /* end while */
+	if (f == 0)
+	{
+		return (0);
+	} /* end if */
+	return (n);
+} /* End main function */

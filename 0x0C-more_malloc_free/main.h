b2dbf902193231c7db825ebb2a10921143a29082
lawal tajudeen <lwltjdn@gmail.com>
2023-06-16 00:09:58 +0100

    header

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,7 @@ void *malloc_checked(unsigned int b);
 /* task #1 */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
 #endif

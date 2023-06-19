d186e732280f8bb15164833fef45fc1cec28f439
lawal tajudeen <lwltjdn@gmail.com>
2023-06-19 13:14:03 +0100

    a function-like macro ABS(x) that computes the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/*
+ * ABS(x) computes the absolute value of a number x
+ * if x is less than 0
+ * Convert it to positive.
+ */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif

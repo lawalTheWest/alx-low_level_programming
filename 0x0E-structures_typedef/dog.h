24c27e7a3ec80353c28d088bf0c766f879fded8e
lawal tajudeen <lwltjdn@gmail.com>
2023-06-19 21:04:17 +0100

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog object template
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t; /* End struct */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *cp_to, char *cp_from);
+int _strlen(char *str);
+
+#endif

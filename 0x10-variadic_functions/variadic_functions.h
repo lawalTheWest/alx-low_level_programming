4222753e22cf87efe9ebfac177790fc8fa8704e9
lawal tajudeen <lwltjdn@gmail.com>
2023-06-23 00:22:18 +0100

    a function that prints anything.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,5 +16,27 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 /* Task #2 */
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/* Task #3 */
+void print_all(const char * const format, ...);
+
+/**
+ * struct printTypeStruct - structure definition of a printTypeStruct
+ * @type: type
+ * @printer: function to print
+ */
+typedef struct formatStruct
+{
+	char *type;
+	void (*printer)(va_list); /* a function pointer */
+} formatStruct;
+
+/* handle character printing */
+void print_char(va_list args);
+/* handle integer printing */
+void print_int(va_list args);
+/* handle float printing */
+void print_float(va_list args);
+/* handle string printing */
+void print_str(va_list args);
 
 #endif

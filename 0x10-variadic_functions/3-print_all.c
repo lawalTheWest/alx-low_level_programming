3acd58edadef629123f84f50b3dd8abd52645ee3
lawal tajudeen <lwltjdn@gmail.com>
2023-06-23 00:20:55 +0100

    a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+
+/**
+ * print_int - handles the intiger values
+ * @args: arguments from print_all
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+} /* End function */
+
+/**
+ * print_float - handles the float values
+ * @args: arguments from print_all
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+} /* End function */
+
+/**
+ * print_char - handles the character values
+ * @args: arguments from print_all
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+} /* End function */
+
+/**
+ * print_str - prints string characters
+ * @args: arguments from print_all
+ */
+void print_str(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	/* Validating argument */
+	if (s == NULL)
+	{
+		printf("(nil)");
+	} /* end if */
+	else
+	{
+		printf("%s", s);
+	} /* end else */
+} /* End function */
+
+/**
+ * print_all - prints any type
+ * @format: arguments to print
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	int j = 0;
+	char *sep = "";
+
+	formatStruct formatStruct1[] = {
+		{ "i", print_int },
+		{ "f", print_float },
+		{ "c", print_char },
+		{ "s", print_str },
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (*formatStruct1[j].type == format[i])
+			{
+				printf("%s", sep);
+				formatStruct1[j].printer(args);
+				sep = ", ";
+				break;
+			} /* End if */
+			j++;
+		} /* End while */
+		i++;
+	} /* End while */
+
+	printf("\n");
+	va_end(args);
+} /* End function */

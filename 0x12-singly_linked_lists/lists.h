62fd8052b63421f4e9505cd5a46d0c2503e65e1d
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-03 16:27:18 +0100

    Header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,28 +3,27 @@
 
 /**
  * struct list_s - singly linked list
- * @str: the string
- * @len: lenght of str
- * @next: pointer to the next node
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for Holberton project
  */
+
 typedef struct list_s
+
 {
 	char *str;
 	unsigned int len;
 
 	struct list_s *next;
 }
-list; /* End structure */
+list_t;
 
 size_t print_list(const list_t *h);
-
-/* task #1 */
 size_t list_len(const list_t *h);
-
 list_t *add_node(list_t **head, const char *str);
-
 list_t *add_node_end(list_t **head, const char *str);
-
 void free_list(list_t *head);
 
 #endif

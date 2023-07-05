b9f1b731fa192a32abe492307df51117a5a40124
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-05 14:52:33 +0100

    Header file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct listint_s: structure (linked list)
+ * @n: integer
+ * @node: a pointer to the next node.
+ */
+typedef struct listint_s
+{
+        int n;
+        struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+
+
+#endif

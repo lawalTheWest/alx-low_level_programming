e1507d7fd3a3809bd3797c0476a438e629bf6ced
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-06 14:31:03 +0100

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,10 +14,26 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/* Task #0 */
 size_t print_listint(const listint_t *h);
+
+/* Task #1 */
 size_t listint_len(const listint_t *h);
+
+/* Task #2 */
 listint_t *add_nodeint(listint_t **head, const int n);
+
+/* Task #3 */
 listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/* Task #4 */
 void free_listint(listint_t *head);
+
+/* Task #5 */
 void free_listint2(listint_t **head);
+
+/* Task #6 */
+int pop_listint(listint_t **head);
+
 #endif

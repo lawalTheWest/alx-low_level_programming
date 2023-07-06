220ec95518663a806264009b10f91915ff0be5ce
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-06 14:47:44 +0100

    header file to add prototype for  a function that returns the nth node of a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,4 +36,8 @@ void free_listint2(listint_t **head);
 /* Task #6 */
 int pop_listint(listint_t **head);
 
+/* Task #7 */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+
 #endif

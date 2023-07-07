af4b2b43ffd54c071d5c5dcfdd4a1cc27377c742
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-07 13:04:44 +0100

    header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,33 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ * Task #0
+ * A function that converts a binary number
+ * to an unsigned int
+ */
+unsigned int binary_to_uint(const char *b);
+
+/*
+ * Task #1
+ * A function that prints
+ * the binary representation of a number.
+ */
+void print_binary(unsigned long int n);
+
+/*
+ * Task #2
+ * A function that returns the value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index);
+
+/*
+ * Task #3
+ * A function that sets the value of a bit to 1 at a given index
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+
+
+#endif

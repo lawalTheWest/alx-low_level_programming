59764b528445016a987c8553b3037a71d023e911
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-10 21:05:21 +0100

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* task #1 */
 int create_file(const char *filename, char *text_content);
 
+/* task #2 */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif

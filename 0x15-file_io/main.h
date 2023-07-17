a21454f1564897cbda60ef9b5615ab92e8e3c4f3
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-07-17 14:07:55 +0100

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,4 +15,9 @@ int create_file(const char *filename, char *text_content);
 /* task #2 */
 int append_text_to_file(const char *filename, char *text_content);
 
+/* task #3 */
+int main(int argc, char *argv[]);
+char *create_buffer(char *fileName);
+void close_file(int fd);
+
 #endif

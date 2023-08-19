41cbdf34c45385b100a23b1fade9d8dc03cc0e5b
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-19 07:32:30 +0100

    header file updated to include the prototype for  a function that frees a dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,4 +31,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 /* Task 3 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
+/* Task 4 */
+void free_dlistint(dlistint_t *head);
 #endif

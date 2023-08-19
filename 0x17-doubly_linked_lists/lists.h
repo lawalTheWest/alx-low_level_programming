3a45bc625323b5ab6b1fb84d0480d7feacd4c5aa
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-19 07:00:28 +0100

    header file updated to add a prototype for  a function that adds a new node at the end of a dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,4 +28,7 @@ size_t dlistint_len(const dlistint_t *h);
 /* Task 2 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
+/* Task 3 */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
 #endif

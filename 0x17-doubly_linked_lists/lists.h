0ecbe4d74bd8cf3355e8fe012d4915e79c2e3915
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-20 21:08:13 +0100

    a header file to include the prototype of a function that deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -43,4 +43,6 @@ int sum_dlistint(dlistint_t *head);
 /* Task 7 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
+/* Task 8 */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 #endif

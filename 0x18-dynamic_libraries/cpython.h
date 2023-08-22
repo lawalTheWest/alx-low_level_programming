96029c75d06fb6b4afc54dc0ea140658bc1fe151
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-22 04:20:13 +0100

    header file for cpython

---
+++ 0x18-dynamic_libraries/cpython.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/* To add two integers */
+int add(int a, int b);
+
+/* To find the differences between two intwgers */
+int sub(int a, int b);
+
+/* To find the products of two integers */
+int mul(int a, int b);
+
+/* To find the floor division of two integers */
+int div(int a, int b);
+
+/* To find the remainder after division of two integers */
+int mod(int a, int b);
+
+#endif

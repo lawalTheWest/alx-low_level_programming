bc79e70dc4c390495161fad3d2c699782c2e68df
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-08-22 04:24:05 +0100

    Header file

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -23,4 +23,20 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+
+/* To add two integers */
+int add(int a, int b);
+
+/* To find the differences between two intwgers */
+int sub(int a, int b);
+
+/* To find the products of two integers */
+int mul(int a, int b);
+
+/* To find the floor division of two integers */
+int div(int a, int b);
+
+/* To find the remainder after division of two integers */
+int mod(int a, int b);
+
 #endif

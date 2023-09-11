60d2c601d1493bf298aa354b8367048e0f80452e
lawal tajudeen ogunsola <lwltjdn@gmail.com>
2023-09-11 04:07:02 +0100

    header file to add a prototype for  a function that retrieves a value associated with a key.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -47,5 +47,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_node(const char *key, const char *value);
 
+/* Task #4 */
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif

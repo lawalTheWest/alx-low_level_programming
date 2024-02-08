4aa47c375bec1cb11aa71c13c1bb4a0a5d6ac162
LawalTheWest <lwltjdn@gmail.com>
2024-02-08 01:59:53 +0100

    a header file to add the prototype for  a function that searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,14 @@
 #define SORTING_ALGOS_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
+/* Task #0 */
 int linear_search(int *array, size_t size, int value);
+
+/* Task #1 */
 int binary_search(int *array, size_t size, int value);
+
+/* Task #7 */
+int jump_search(int *array, size_t size, int value);
 #endif

10082423f9bc6b1f9f2e0be1e7118eec52f1fe8a
LawalTheWest <lwltjdn@gmail.com>
2024-04-11 20:52:22 +0100

    fixed white trailing space to match the betty coding style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,29 +2,29 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-        int i;
+	int i;
 	int lt = 0;
 	int rt;
 
 	/* Validate array */
-        if (!(array))
-                return (-1);
+	if (!(array))
+		return (-1);
 
-        for (lt = 0, rt = size - 1; rt >= lt;)
-        {
-                printf("Searching in array: ");
-                for (i = lt; i < rt; i++)
-                        printf("%d, ", array[i]);
-                printf("%d\n", array[i]);
+	for (lt = 0, rt = size - 1; rt >= lt;)
+	{
+		printf("Searching in array: ");
+		for (i = lt; i < rt; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-                i = lt + (rt - lt) / 2;
-                if (array[i] == value)
-                        return (i);
-                if (array[i] > value)
-                        rt = i - 1;
-                else
-                        lt = i + 1;
-        }
+		i = lt + (rt - lt) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			rt = i - 1;
+		else
+			lt = i + 1;
+	}
 
-        return (-1);
-}
+	return (-1);
+} /* End function */
